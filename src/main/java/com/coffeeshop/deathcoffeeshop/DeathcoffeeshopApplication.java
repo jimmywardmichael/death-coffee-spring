adae06572eadecfa783ff37769530f424e04ab4f
James Ward <jimmywardmichael@gmail.com>
2023-08-30 13:20:07 -0500

    init

---
+++ src/main/java/com/coffeeshop/deathcoffeeshop/DeathcoffeeshopApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.deathcoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeathcoffeeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeathcoffeeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeeshop/deathcoffeeshop/DeathcoffeeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.deathcoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeathcoffeeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
