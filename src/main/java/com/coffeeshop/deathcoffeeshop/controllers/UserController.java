a72a599121097b73314e74dd862257835e2ac46a
jimmywardmichael <jimmywardmichael@gmail.com>
2024-07-14 22:11:09 -0500

    currently fixing db

--- src/main/java/com/coffeeshop/deathcoffeeshop/controllers/UserController.java
+++ src/main/java/com/coffeeshop/deathcoffeeshop/controllers/UserController.java
@@ -13,28 +13,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-//Dependency injection
+    // Dependency injection
     private final UserRepository userDao;
+
     @Autowired
-public UserController(UserRepository userDao){
+    public UserController(UserRepository userDao) {
         this.userDao = userDao;
     }
+
     @GetMapping("/signup")
-public String showSignUpForm(Model model) {
-   // Create a new User object
-    model.addAttribute("user",new User()); // Add the 'user' object to the model
-    return "html/signup";
-}
-   @PostMapping("/signup")
-    public String processSignUpForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            // If there are validation errors, return to the signup form
-            return "html/signup";
-        }
-
-        // Save the user to the database
-        userDao.save(user);
-
-        return "html/coffeehome";
+    public String showSignUpForm(Model model) {
+        // Create a new User object
+        model.addAttribute("user", new User()); // Add the 'user' object to the model
+        return "html/signup";
     }
+
+    @PostMapping("/signup")
+public String processSignUpForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+        return "html/signup";
+    }
+
+    userDao.save(user); // This uses your UserRepository to save to MongoDB
+
+    return "html/coffeehome";
+}
+//    @PostMapping("/signup")
+//    public String processSignUpForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//            // If there are validation errors, return to the signup form
+//            return "html/signup";
+//        }
+//
+//        // Save the user to the database
+//        userDao.save(user);
+//
+//        return "html/coffeehome";
+//    }
 }
